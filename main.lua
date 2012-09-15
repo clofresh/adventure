2fb50af40e97468d9e61121eedc4e1929ddb8efc
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2012-09-15 16:28:12 -0400

    encapsulate player update and draw functions

--- main.lua
+++ main.lua
@@ -1,7 +1,21 @@
 local anim8 = require 'anim8'
 local HC = require 'lib/HardonCollider'
 
-local image, animation, player, Collider, dummy
+local player, Collider, dummy, sprites
+sprites = {}
+
+function initSprite(image, startPos, startDir, startState, startAnimation, animation, updateFunc, drawFunc)
+  return {
+    image = image,
+    pos = startPos,
+    direction = startDir,
+    currentState = startState,
+    currentAnimation = startAnimation,
+    animation = animation,
+    update = updateFunc,
+    draw = drawFunc,
+  }
+end
 
 function onCollide(dt, shapeA, shapeB)
   local p, d
@@ -23,94 +37,118 @@ function onCollide(dt, shapeA, shapeB)
 end
 
 function love.load()
-  Collider = HC(100, onCollide)
-  image = love.graphics.newImage('rivercityransom_alex_sheet.png')
-  local g = anim8.newGrid(40, 40, image:getWidth(), image:getHeight())
-  player = {
-    animation = {
+  local alex, ryan
+  alex = love.graphics.newImage('rivercityransom_alex_sheet.png')
+  ryan = love.graphics.newImage('rivercityransom_ryan_sheet.png')
+  sprites.alex = {image=alex}
+  local g = anim8.newGrid(40, 40, alex:getWidth(), alex:getHeight())
+  sprites.alex.animation = {
       idle = anim8.newAnimation('once', g('1,1'), 0.1),
       walking = anim8.newAnimation('loop', g('2-3,1'), 0.1),
       punching = anim8.newAnimation('once', g('1-3,2'), 0.1),
       uppercutting = anim8.newAnimation('once', g('7-9,2'), 0.1, {0.2, 0.1, 0.2}),
       sidekicking = anim8.newAnimation('once', g('1-3,3'), 0.1),
       highkicking = anim8.newAnimation('once', g('1-4,3'), 0.1),
-    },
-    pos = {x=100, y=200},
-    direction = 1,
-    currentState = 'idle'
-  }
-  player.currentAnimation = player.animation.idle
-  dummy = Collider:addRectangle(200, 200, 100, 100)
+    }
+  sprites.ryan = {image=ryan}
+  local g = anim8.newGrid(40, 40, ryan:getWidth(), ryan:getHeight())
+  sprites.ryan.animation = {
+      idle = anim8.newAnimation('once', g('1,1'), 0.1),
+      walking = anim8.newAnimation('loop', g('2-3,1'), 0.1),
+      punching = anim8.newAnimation('once', g('1-3,2'), 0.1),
+      uppercutting = anim8.newAnimation('once', g('7-9,2'), 0.1, {0.2, 0.1, 0.2}),
+      sidekicking = anim8.newAnimation('once', g('1-3,3'), 0.1),
+      highkicking = anim8.newAnimation('once', g('1-4,3'), 0.1),
+    }
+
+  player = initSprite(sprites.alex.image, {x=200, y=300}, 1, 'idle',
+    sprites.alex.animation.idle, sprites.alex.animation,
+    function(self, dt)
+      if self.currentState == 'idle' or self.currentState == 'walking' then
+        if love.keyboard.isDown("u") then
+          self.currentAnimation = self.animation.uppercutting:clone()
+          self.currentState = 'attacking'
+        elseif love.keyboard.isDown("i") then
+          self.currentAnimation = self.animation.punching:clone()
+          self.currentState = 'attacking'
+          self.attackRegion = Collider:addCircle(self.pos.x + 24, self.pos.y + 12, 4)
+        elseif love.keyboard.isDown("j") then
+          self.currentAnimation = self.animation.highkicking:clone()
+          self.currentState = 'attacking'
+        elseif love.keyboard.isDown("k") then
+          self.currentAnimation = self.animation.sidekicking:clone()
+          self.currentState = 'attacking'
+        else
+          local pressed = false
+          if love.keyboard.isDown("w") then
+            self.pos.y = self.pos.y - 1
+            pressed = true
+          end
+          if love.keyboard.isDown("s") then
+            self.pos.y = self.pos.y + 1
+            pressed = true
+          end
+          if love.keyboard.isDown("a") then
+            self.pos.x = self.pos.x - 1
+            pressed = true
+            self.direction = -1
+          end
+          if love.keyboard.isDown("d") then
+            self.pos.x = self.pos.x + 1
+            pressed = true
+            self.direction = 1
+          end
+
+
+          if pressed then
+            self.currentAnimation = self.animation.walking
+            self.currentState = 'walking'
+          else
+            self.currentAnimation = self.animation.idle
+            self.currentState = 'idle'
+          end
+        end
+      elseif self.currentState == 'attacking' and self.currentAnimation.status == 'finished' then
+        self.currentAnimation = self.animation.idle
+        self.currentState = 'idle'
+        self.attackRegion = nil
+      end
+
+      self.currentAnimation:update(dt)
+
+    end,
+    function(self)
+      local ox, oy
+      ox, oy = 0, 0
+      -- need to compensate for the image not being centered when flipping
+      if self.direction == -1 then
+        ox = 24
+        oy = 0
+      end
+      self.currentAnimation:draw(self.image, self.pos.x, self.pos.y, 0, self.direction, 1, ox, oy)
+      if self.attackRegion then
+        self.attackRegion:draw('fill', 16)
+      end
+    end)
+  dummy = initSprite(sprites.ryan.image, {x=400, y=300}, -1, 'idle',
+    sprites.ryan.animation.idle, sprites.ryan.animation,
+    function(self, dt)
+    end,
+    function(self)
+    end)
+
+  Collider = HC(100, onCollide)
 end
 
 function love.update(dt)
-  if player.currentState == 'idle' or player.currentState == 'walking' then
-    if love.keyboard.isDown("u") then
-      player.currentAnimation = player.animation.uppercutting:clone()
-      player.currentState = 'attacking'
-    elseif love.keyboard.isDown("i") then
-      player.currentAnimation = player.animation.punching:clone()
-      player.currentState = 'attacking'
-      player.attackRegion = Collider:addCircle(player.pos.x + 24, player.pos.y + 12, 4)
-    elseif love.keyboard.isDown("j") then
-      player.currentAnimation = player.animation.highkicking:clone()
-      player.currentState = 'attacking'
-    elseif love.keyboard.isDown("k") then
-      player.currentAnimation = player.animation.sidekicking:clone()
-      player.currentState = 'attacking'
-    else
-      local pressed = false
-      if love.keyboard.isDown("w") then
-        player.pos.y = player.pos.y - 1
-        pressed = true
-      end
-      if love.keyboard.isDown("s") then
-        player.pos.y = player.pos.y + 1
-        pressed = true
-      end
-      if love.keyboard.isDown("a") then
-        player.pos.x = player.pos.x - 1
-        pressed = true
-        player.direction = -1
-      end
-      if love.keyboard.isDown("d") then
-        player.pos.x = player.pos.x + 1
-        pressed = true
-        player.direction = 1
-      end
-
-
-      if pressed then
-        player.currentAnimation = player.animation.walking
-        player.currentState = 'walking'
-      else
-        player.currentAnimation = player.animation.idle
-        player.currentState = 'idle'
-      end
-    end
-  elseif player.currentState == 'attacking' and player.currentAnimation.status == 'finished' then
-    player.currentAnimation = player.animation.idle
-    player.currentState = 'idle'
-    player.attackRegion = nil
-  end
-
-  player.currentAnimation:update(dt)
+  player:update(dt)
+  dummy:update(dt)
   Collider:update(dt)
 end
 
 function love.draw()
-  local ox, oy
-  ox, oy = 0, 0
-  -- need to compensate for the image not being centered when flipping
-  if player.direction == -1 then
-    ox = 24
-    oy = 0
-  end
-  player.currentAnimation:draw(image, player.pos.x, player.pos.y, 0, player.direction, 1, ox, oy)
-  if player.attackRegion then
-    player.attackRegion:draw('fill', 16)
-  end
-  dummy:draw('fill')
+  player:draw()
+  dummy:draw()
 end
 
 
